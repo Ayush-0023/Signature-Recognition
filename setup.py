a923b62ea5482a818c9877d3b6238927e47c6cc2
Ayush-0023 <ayush99coding@gmail.com>
2026-08-05 13:58:21 +0530

    Add notebook

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='ayush',
+    author_email='ayush99coding@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
